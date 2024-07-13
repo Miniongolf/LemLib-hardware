c3cf875b1ffb14f97328a744977712cbd9ce10df
SizzinSeal <liamteale6@gmail.com>
2024-07-12 17:04:55 -0700

    :sparkles: implement Encoder

--- include/hardware/encoder/encoder.hpp
+++ include/hardware/encoder/encoder.hpp
@@ -1,1 +1,86 @@
 #pragma once
+
+#include "units/Angle.hpp"
+
+namespace lemlib {
+class Encoder {
+    public:
+        /**
+         * @brief reset the encoder
+         *
+         * This function resets the encoder. This function is non-blocking.
+         */
+        virtual void calibrate() = 0;
+        /**
+         * @brief check if the encoder is calibrated
+         *
+         * @return true the encoder is calibrated
+         * @return false the encoder is not calibrated
+         */
+        virtual bool isCalibrated() = 0;
+        /**
+         * @brief check if the encoder is calibrating
+         *
+         * @return true the encoder is calibrating
+         * @return false the encoder is not calibrating
+         */
+        virtual bool isCalibrating() = 0;
+        /**
+         * @brief whether the encoder is connected
+         *
+         * @return true the encoder is connect
+         * @return false the encoder is not connected
+         */
+        virtual bool isConnected() = 0;
+        /**
+         * @brief Get the absolute angle measured by the encoder
+         *
+         * The absolute angle measured by the encoder is the physical angle of the encoder. As such, it is bounded
+         * between 0 and 360 degrees.
+         *
+         * @return Angle
+         */
+        virtual Angle getAbsoluteAngle() = 0;
+        /**
+         * @brief Get the relative angle measured by the encoder
+         *
+         * The relative angle measured by the encoder is the angle of the encoder relative to the last time the encoder
+         * was reset. As such, it is unbounded.
+         *
+         * @return Angle
+         */
+        virtual Angle getRelativeAngle() = 0;
+        /**
+         * @brief Set the absolute angle of the encoder
+         *
+         * This function sets the absolute angle of the encoder. This function is non-blocking.
+         *
+         * @param angle
+         */
+        virtual void setAbsoluteAngle(Angle angle) = 0;
+        /**
+         * @brief Set the relative angle of the encoder
+         *
+         * This function sets the relative angle of the encoder. This function is non-blocking.
+         *
+         * @param angle
+         */
+        virtual void setRelativeAngle(Angle angle) = 0;
+        /**
+         * @brief Set the encoder to reversed
+         *
+         * This function sets the encoder to reversed. This function is non-blocking.
+         *
+         * @param reversed
+         */
+        virtual void setReversed(bool reversed) = 0;
+        /**
+         * @brief Check if the encoder is reversed
+         *
+         * @return true the encoder is reversed
+         * @return false the encoder is not reversed
+         */
+        virtual bool isReversed() = 0;
+        virtual ~Encoder() = default;
+};
+} // namespace lemlib
