355e77798f6c8ab6d01b8a2025af0e73ac2ef09e
SizzinSeal <liamteale6@gmail.com>
2024-07-25 15:12:14 -0600

    implement Motor::setAngle

--- src/hardware/Motor.cpp
+++ src/hardware/Motor.cpp
@@ -1,7 +1,30 @@
 #include "hardware/Motor.hpp"
 #include "pros/abstract_motor.hpp"
+#include "units/Angle.hpp"
+#include <climits>
 
 namespace lemlib {
+
+pros::MotorBrake brakeModeToMotorBrake(BrakeMode mode) {
+    // pros::MotorBrake is identical to lemlib::BrakeMode, except for its name and lemlib uses an enum class for type
+    // safety
+    switch (mode) {
+        case BrakeMode::COAST: return pros::MotorBrake::coast;
+        case BrakeMode::BRAKE: return pros::MotorBrake::brake;
+        case BrakeMode::HOLD: return pros::MotorBrake::hold;
+        default: return pros::MotorBrake::invalid;
+    }
+}
+
+BrakeMode motorBrakeToBrakeMode(pros::MotorBrake mode) {
+    switch (mode) {
+        case pros::MotorBrake::coast: return BrakeMode::COAST;
+        case pros::MotorBrake::brake: return BrakeMode::BRAKE;
+        case pros::MotorBrake::hold: return BrakeMode::HOLD;
+        default: return BrakeMode::INVALID;
+    }
+}
+
 Motor::Motor(pros::Motor motor)
     : m_motor(motor) {}
 
@@ -16,12 +39,70 @@ int Motor::move(double percent) {
     }
 }
 
+int Motor::moveVelocity(AngularVelocity velocity) {
+    // pros uses an integer value to represent the rpm of the motor
+    return m_motor.move_velocity(to_rpm(units::round(velocity, rpm)));
+}
+
+int Motor::brake() { return m_motor.brake(); }
+
+int Motor::setBrakeMode(BrakeMode mode) { return m_motor.set_brake_mode(brakeModeToMotorBrake(mode)); }
+
+BrakeMode Motor::getBrakeMode() { return motorBrakeToBrakeMode(m_motor.get_brake_mode()); }
+
+int Motor::isConnected() { return m_motor.is_installed(); }
+
+Angle Motor::getAngle() {
+    // to allow the user to use whatever encoder units they want, we only deal with raw encoder counts
+    // and adjust for different cartridges ourselves
+    const pros::MotorGears cartridge = m_motor.get_gearing();
+    const int counts = m_motor.get_raw_position(NULL);
+    if (counts == INT_MAX) return from_sDeg(INFINITY);
+    switch (cartridge) {
+        // without a cartridge, the encoder measures 50 counts per revolution
+        // so we multiply the number of ticks by the gear ratio of the cartridge, and then by 360 to get the
+        // value in degrees
+        case pros::MotorGears::blue: return from_sDeg(counts * 6 * 360);
+        case pros::MotorGears::green: return from_sDeg(counts * 18 * 360);
+        case pros::MotorGears::red: return from_sDeg(counts * 36 * 360);
+        default: return from_sDeg(INFINITY);
+    }
+}
+
+int Motor::setAngle(Angle angle) {
+    // PROS does not let us just set the position, or let us set the zero position using raw encoder ticks
+    // one solution is to temporarily set the encoder units, but writing ops open us up to race conditions
+    // and other problems which aren't fun to deal with
+    // so instead, we account for all the possible set encoder units
+    const pros::MotorUnits units = m_motor.get_encoder_units();
+    const double raw = m_motor.get_position();
+    if (units == pros::MotorUnits::invalid) return INT_MAX;
+    if (raw == INFINITY) return INFINITY;
+    switch (units) {
+        case pros::MotorUnits::counts: {
+            const pros::MotorGears cartridge = m_motor.get_gearing();
+            if (cartridge == pros::MotorGears::invalid) return INT_MAX;
+            switch (cartridge) {
+                // without a cartridge, the encoder measures 50 counts per revolution
+                // so we multiply the requested angle in rotations to the number of ticks per rotation
+                case pros::MotorGears::blue: return m_motor.set_zero_position(raw - (to_sDeg(angle) / 360) * 300);
+                case pros::MotorGears::green: return m_motor.set_zero_position(raw - (to_sDeg(angle) / 360) * 900);
+                case pros::MotorGears::red: return m_motor.set_zero_position(raw - (to_sDeg(angle) / 360) * 1800);
+                default: return INT_MAX;
+            }
+        }
+        case pros::MotorUnits::degrees: return m_motor.set_zero_position(raw - to_sDeg(angle));
+        case pros::MotorUnits::rotations: return m_motor.set_zero_position(raw - to_sDeg(angle) / 360);
+        default: return INT_MAX;
+    }
+}
+
 MotorType Motor::getType() {
     // there is no exposed api to get the motor type
-    // while the memory address of the private member variable has been found through reverse engineering,
+    // while the memory address of the function has been found through reverse engineering,
     // it may break between VEXos updates. Instead, we see if we can change the cartridge to something other
     // than the green cartridge, which is only possible on the V5 motor
-    const auto oldCart = m_motor.get_gearing();
+    const pros::MotorGears oldCart = m_motor.get_gearing();
     // check for errors
     if (oldCart == pros::MotorGears::invalid) return MotorType::INVALID;
     m_motor.set_gearing(pros::v5::MotorGears::red);
