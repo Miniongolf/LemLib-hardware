bfd81ef01bbe395f6edf471eaa528a783265eb08
Liam Teale <liamteale6@gmail.com>
2024-11-20 14:17:04 -0800

    format Port.hpp, use pragma once

--- include/hardware/Port.hpp
+++ include/hardware/Port.hpp
@@ -1,52 +1,57 @@
+#pragma once
+
 #include <cstdint>
 
 namespace lemlib {
 
 struct DynamicPort {};
 
-constexpr DynamicPort runtime_check_port{};
+constexpr DynamicPort runtime_check_port {};
 
 class SmartPort {
     public:
-        consteval SmartPort(std::int64_t port): m_port(port) {
+        consteval SmartPort(std::int64_t port)
+            : m_port(port) {
             // XXX: this doesnt actaully throw an exception since the ctor
             // is consteval, it halts compilation instead
             if (port < 1 || port > 21) throw "Port out of range!";
         }
-        SmartPort(std::int64_t port, DynamicPort) : m_port(port) {
+
+        SmartPort(std::int64_t port, DynamicPort)
+            : m_port(port) {
             if (port < 1 || port > 21) m_port = 0;
         }
-        std::uint8_t get_port() const {
-            return m_port;
-        }
-        operator std::uint8_t() const {
-            return m_port;
-        }
+
+        std::uint8_t get_port() const { return m_port; }
+
+        operator std::uint8_t() const { return m_port; }
     private:
         std::uint8_t m_port;
 };
 
 class ReversibleSmartPort {
     public:
-        consteval ReversibleSmartPort(std::int64_t port): m_port(port < 0 ? -port : port), m_reversed(port < 0) {
-            if (port < 0) port = -port;  
+        consteval ReversibleSmartPort(std::int64_t port)
+            : m_port(port < 0 ? -port : port),
+              m_reversed(port < 0) {
+            if (port < 0) port = -port;
             // XXX: this doesnt actaully throw an exception since the ctor
             // is consteval, it halts compilation instead
             if (port < 1 || port > 21) throw "Port out of range!";
         }
-        ReversibleSmartPort(std::int64_t port, DynamicPort): m_port(port < 0 ? -port : port), m_reversed(port < 0) {
+
+        ReversibleSmartPort(std::int64_t port, DynamicPort)
+            : m_port(port < 0 ? -port : port),
+              m_reversed(port < 0) {
             if (port < 0) port = -port;
             if (port < 1 || port > 21) m_port = 0;
         }
-        std::uint8_t get_port() const {
-            return m_port;
-        }
-        bool get_reversed() const {
-            return m_reversed;
-        }
-        operator int() const {
-            return m_port;
-        }
+
+        std::uint8_t get_port() const { return m_port; }
+
+        bool get_reversed() const { return m_reversed; }
+
+        operator int() const { return m_port; }
     private:
         std::uint8_t m_port;
         bool m_reversed;
@@ -54,27 +59,28 @@ class ReversibleSmartPort {
 
 class ADIPort {
     public:
-        consteval ADIPort(std::int64_t port): m_port(0) {
+        consteval ADIPort(std::int64_t port)
+            : m_port(0) {
             if (port >= 'a' && port <= 'h') port -= ('a' - 1);
             else if (port >= 'A' && port <= 'H') port -= ('A' - 1);
             if (port < 1 || port > 8) throw "Port out of range!";
             m_port = port;
         }
-        ADIPort(std::int64_t port, DynamicPort) : m_port(0) {
+
+        ADIPort(std::int64_t port, DynamicPort)
+            : m_port(0) {
             if (port < 1 || port > 21) m_port = 0;
             if (port >= 'a' && port <= 'h') port -= ('a' - 1);
             else if (port >= 'A' && port <= 'H') port -= ('A' - 1);
             if (port < 1 || port > 8) return;
             m_port = port;
         }
-        std::uint8_t get_port() const {
-            return m_port;
-        }
-        operator std::uint8_t() const {
-            return m_port;
-        }
+
+        std::uint8_t get_port() const { return m_port; }
+
+        operator std::uint8_t() const { return m_port; }
     private:
         std::uint8_t m_port;
 };
 
-}
+} // namespace lemlib
